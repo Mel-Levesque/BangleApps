331c365c8da27082c2cf5bbd151b489721e7acea
melim <lvsqmelanie@gmail.com>
2024-01-02 13:31:43 +0100

    Add first version FTR

--- apps/followtherecipe/README.md
+++ apps/followtherecipe/README.md
@@ -1,6 +1,25 @@
 # Follow the recipe
-A simple app using Gadgedtbridge internet access to fetch a recipe and follow it step by step.
+A simple app using Gadgetbridge internet access to fetch a recipe and follow it step by step.
+
+For now, it display a random recipe whenever you restart the app.
+Go to the next screen via tab right and go the previous screen via tab left.
+
+Make sure that you allowed 'Internet Access' via the Gadgetbridge app before using Follow The Recipe.
+
+If you run the app via web IDE, connect your Banglejs via Gadgetbridge app then in the web IDE connect via Android.
+For more informations, [see the documentation about Gadgetbridge](https://www.espruino.com/Gadgetbridge)
 
 TO-DOs:
-- [X] Add random recipe
-- [] other
+
+- [X] Display random recipe on start
+- [ ] Choose a recipe by web page
+- [ ] improve GUI (color, fonts, ...)
+
+## Contributors
+
+Written by [Mel-Levesque](https://github.com/Mel-Levesque)
+
+## Thanks To
+
+- Design taken from the [Info application](https://github.com/espruino/BangleApps/tree/master/apps/info) by [David Peer](https://github.com/peerdavid)
+- App icon from [icons8.com](https://icons8.com)
--- apps/followtherecipe/app.js
+++ apps/followtherecipe/app.js
@@ -5,129 +5,23 @@ var W = g.getWidth(), H = g.getHeight();
 var screen = 0;
 var Layout = require("Layout");
 
-// String variables
-let mealName = "";
-let category = "";
-let area = "";
-let instructions = "";
-let mealImg = "";
+let maxLenghtHorizontal = 16;
+let maxLenghtvertical = 6;
 
-let uri = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
-
-if (Bangle.http){
-  Bangle.http(uri, {timeout:10000}).then(event => {
-    console.log("event = "+event);
-    console.log("event.resp = "+event.resp);
-    let result = event.resp;
-  }).catch((e)=>{
-    console.log("Request Error = "+e);
-  });
-} else {
-  console.log("No http method found");
-}
-
-//get json format and parse it into Strings
-function getRecipeData(data){
-  mealName = data.meal[0].strMeal;
-  category = data.meal[0].strCategory;
-  category = data.meal[0].strArea;
-  instructions = data.meal[0].strInstructions;
-}
-
-var ingredients1 = "Penne rigate: 1 pound\nOlive oil: 1/4 cup\nGarlic: 3 cloves\nChopped tomatoes: \n1 tin\nRed chile flakes: \n1/2 teaspoon";
-
-var ingredients2 = "Italian seasoning: \n1/2 teaspoon\nBasil: 6 leaves\nParmigiano-Reggiano: \nspinkling";
+let uri = "https://www.themealdb.com/api/json/v1/1/search.php?s=Lamb_Tagine";
 
 var colors = {0: "#70f", 1:"#70d", 2: "#70g", 3: "#20f", 4: "#30f"};
 
-var screens = [
-  {
-    name: "General",
-    items: [
-      {name: "Spicy Arrabiata", fun:  () => ""},
-      {name: "", fun:  () => ""},
-      {name: "Type: ", fun:  () => splitString("Vege")},
-      {name: "Origin: ", fun:  () => splitString("Italian")},
-      {name: "", fun:  () => ""},
-      {name: "Image: ", fun:  () => getWeatherTemp()},
-    ]
-  },
-  {
-    name: "Ingredients",
-    items: [
-      {name: "Penne rigate: 1 pound", fun:  () => ""},
-      {name: "Olive oil: 1/4 cup", fun:  () => ""},
-      {name: "Garlic: 3 cloves", fun:  () => ""},
-      {name: "Chopped tomatoes: \n1 tin", fun:  () => ""},
-      {name: "\nRed chile flakes: \n1/2 teaspoon", fun:  () => ""},
-    ]
-  },
-  {
-    name: "Ingredients",
-    items: [
-      {name: ingredients1, fun:  () => ""},
-    ]
-  },
-  {
-    name: "Ingredients",
-    items: [
-      {name: ingredients2, fun:  () => ""},
-    ]
-  },
-  {
-    name: "Recette",
-    items: [
-      {name: "Firmw.", fun: () => ENV.VERSION},
-      {name: "Git", fun: () => ENV.GIT_COMMIT},
-    ]
-  }
-];
-
-
-function getWeatherTemp(){
-  try {
-    var weather = storage.readJSON('weather.json').weather;
-    return locale.temp(weather.temp-273.15);
-  } catch(ex) { }
-
-  return "?";
-}
-
-//line break if 8 characters and if more than 24, replaces the remaining characters by a dot
-function splitString(inputString) {
-  let result = '';
-  let interval1 = 8;
-  let interval2 = 24;
-  let replaceChar = ".";
-  
-  if (inputString.length >= interval1) {
-    // Insert line break after every 8 characters
-    result += inputString.substring(0, interval1) + '\n';
-  }
-
-  if (inputString.length >= interval2) {
-    // Truncate the string after 16 characters and add a dot
-    result += inputString.substring(interval1, interval2) + '.';
-  }else if (inputString.length > interval1) {
-    // If the string is between 8 and 24 characters, include the remaining characters
-    result += inputString.substring(interval1);
-  }else{
-    result = inputString;
-  }
-
-  return result;
-}
-
+var screens = [];
 
 function drawData(name, value, y){
   g.drawString(name, 10, y);
   g.drawString(value, 100, y);
 }
 
-
 function drawInfo() {
   g.reset().clearRect(Bangle.appRect);
-  var h=18, y = h;//-h;
+  var h=18, y = h;
 
   // Header
   g.drawLine(0,25,W,25);
@@ -136,13 +30,12 @@ function drawInfo() {
   // Info body depending on screen
   g.setFont("Vector",15).setFontAlign(-1,-1).setColor("#0ff");
   screens[screen].items.forEach(function (item, index){
-    console.log("index = "+index);
     g.setColor(colors[index]);
-    drawData(item.name, item.fun(), y+=h);
+    drawData(item.name, item.fun, y+=h);
   });
 
   // Bottom
-  g.setColor("#000");
+  g.setColor(g.theme.fg);
   g.drawLine(0,H-h-3,W,H-h-3);
   g.drawLine(0,H-h-2,W,H-h-2);
   g.setFont("Vector",h-2).setFontAlign(-1,-1);
@@ -150,8 +43,6 @@ function drawInfo() {
   g.setFont("Vector",h-2).setFontAlign(1,-1);
   g.drawString((screen+1) + "/" + screens.length, W, H-h+2);
 }
-
-drawInfo();
 
 // Change page if user touch the left or the right of the screen
 Bangle.on('touch', function(btn, e){
@@ -173,30 +64,157 @@ Bangle.on('touch', function(btn, e){
   drawInfo();
 });
 
-// Example usage
-//const originalString = "12345678123456781234567812345678";
-//const stringWithLineBreaksAndReplace = insertLineBreaksAndReplace(originalString);
+function infoIngredients(ingredients, measures){
+  let combinedList = [];
+  let listOfString = [];
+  let lineBreaks = 0;
 
-//console.log(stringWithLineBreaksAndReplace);
+  // Iterate through the arrays and combine the ingredients and measures
+  for (let i = 0; i < ingredients.length; i++) {
+    const combinedString = `${ingredients[i]}: ${measures[i]}`;
+    lineBreaks += 1;
+    // Check if the line is more than 16 characters
+    if (combinedString.length > maxLenghtHorizontal) {
+      // Add line break and update lineBreaks counter
+      combinedList.push(`${ingredients[i]}:\n${measures[i]}`);
+      lineBreaks += 1;
+    } else {
+      // Add to the combinedList array
+      combinedList.push(combinedString);
+    }
+    
+    // Check the total line breaks
+    if (lineBreaks >= maxLenghtvertical) {
+      const resultString = combinedList.join('\n');
+      listOfString.push(resultString);
+      combinedList = [];
+      lineBreaks = 0;
+    }
+    if(i == ingredients.length){
+      listOfString.push(combinedList.join('\n'));
+    }
+  }
+  
+  for(let i = 0; i < listOfString.length; i++){
+    let screen = {
+    name: "Ingredients",
+    items: [
+      {name: listOfString[i], fun:  ""},
+    ]
+    };
+    screens.push(screen);
+  }
+}
+
+// Format instructions to display on screen
+function infoInstructions(instructionsString){
+  let item = [];
+  let chunkSize = 22;
+  //remove all space line and other to avoid problem with text
+  instructionsString = instructionsString.replace(/[\n\r]/g, '');
+  
+  for (let i = 0; i < instructionsString.length; i += chunkSize) {
+    const chunk = instructionsString.substring(i, i + chunkSize).trim();
+    item.push({ name: chunk, fun: "" });
+
+    if (item.length === maxLenghtvertical) {
+      let screen = {
+        name: "Instructions",
+        items: item,
+      };
+      screens.push(screen);
+      item = [];
+    }
+  }
+
+  if (item.length > 0) {
+    let screen = {
+      name: "Instructions",
+      items: item,
+    };
+    screens.push(screen);
+  }
+}
 
 
-/*var layout = new Layout( {
-  type:"v", c: [
-    {type:"h", c: [
-       {type:"txt", font:"10%", label:"Spicy Arrabiata", fillx:1, cb: l=>console.log("Change recipe ?") },
-    ]},
-      {type:"txt", font:"5%", label:"Type: Vegetarian", fillx:0},
-      {type:"txt", font:"5%", label:"penne rigate: 1 pound", fillx:0},
-      {type:"txt", font:"5%", label:"olive oil: 1/4 cup", fillx:1},
-      {type:"txt", font:"5%", label:"garlic: 3 cloves", fillx:1},
-    {type:"h", c: [
-      {type:"btn", font:"6x8:2", label:"Prev", fillx:1, cb: l=>console.log("Prev") },
-      {type:"btn", font:"6x8:2", label:"Next", fillx:1, cb: l=>console.log("Next") }
-    ]},
-  ]
-});
+// Get json format and parse it into Strings
+function getRecipeData(data) {
+  let mealName = data.strMeal;
+  let category = data.strCategory;
+  let area = data.strArea;
+  let instructions = data.strInstructions;
+  const ingredients = [];
+  const measures = [];
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = data["strIngredient" + i];
+    const measure = data["strMeasure" + i];
+    if (ingredient && ingredient.trim() !== "") {
+      ingredients.push(ingredient);
+      if (measure && measure.trim() !== ""){
+        measures.push(measure);
+      }else{
+        measures.push("¯\\_(ツ)_/¯");
+      }
+    } else { // If no more ingredients are found
+      screens = [
+        {
+          name: "General",
+          items: [
+            {name: mealName, fun:  ""},
+            {name: "", fun:  ""},
+            {name: "Category", fun:  category},
+            {name: "", fun:  ""},
+            {name: "Area: ", fun:  area},
+          ]
+        }
+      ];
+      infoIngredients(ingredients, measures);
+      infoInstructions(instructions);
+      drawInfo();
+      break;
+    }
+  }
+}
 
-layout.render();*/
+function jsonData(){
+  const json = '{"meals":[{"idMeal":"52771","strMeal":"Spicy Arrabiata Penne","strDrinkAlternate":null,"strCategory":"Vegetarian","strArea":"Italian","strInstructions":"Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\\r\\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\\r\\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.","strMealThumb":"https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg","strTags":"Pasta,Curry","strYoutube":"https://www.youtube.com/watch?v=1IszT_guI08","strIngredient1":"penne rigate","strIngredient2":"olive oil","strIngredient3":"garlic","strIngredient4":"chopped tomatoes","strIngredient5":"red chile flakes","strIngredient6":"italian seasoning","strIngredient7":"basil","strIngredient8":"Parmigiano-Reggiano","strIngredient9":"","strIngredient10":"","strIngredient11":"","strIngredient12":"","strIngredient13":"","strIngredient14":"","strIngredient15":"","strIngredient16":null,"strIngredient17":null,"strIngredient18":null,"strIngredient19":null,"strIngredient20":null,"strMeasure1":"1 pound","strMeasure2":"1/4 cup","strMeasure3":"3 cloves","strMeasure4":"1 tin ","strMeasure5":"1/2 teaspoon","strMeasure6":"1/2 teaspoon","strMeasure7":"6 leaves","strMeasure8":"spinkling","strMeasure9":"","strMeasure10":"","strMeasure11":"","strMeasure12":"","strMeasure13":"","strMeasure14":"","strMeasure15":"","strMeasure16":null,"strMeasure17":null,"strMeasure18":null,"strMeasure19":null,"strMeasure20":null,"strSource":null,"strImageSource":null,"strCreativeCommonsConfirmed":null,"dateModified":null}]}';
+  const obj = JSON.parse(json);
+
+  getRecipeData(obj.meals[0]);
+}
+
+function initData(retryCount) {
+  if (!Bangle.http) {
+    console.log("No http method found");
+    jsonData();
+    return;
+  }
+
+  Bangle.http(uri, { timeout: 1000 })
+    .then(event => {
+      try {
+        const obj = JSON.parse(event.resp);
+
+        if (obj.meals && obj.meals.length > 0) {
+          getRecipeData(obj.meals[0]);
+        } else {
+          console.log("Invalid JSON structure: meals array is missing or empty");
+        }
+      } catch (error) {
+        console.log("JSON Parse Error: " + error.message);
+      }
+    })
+    .catch(e => {
+      console.log("Request Error:", e);
+      if (e === "Timeout" && retryCount > 0) {
+        setTimeout(() => initData(retryCount - 1), 1000); // Optional: Add a delay before retrying
+      }else{
+        jsonData();
+      }
+    });
+}
+
+initData(3);
 
 
 Bangle.on('lock', function(isLocked) {
